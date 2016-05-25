abf567580500ac89e246134fa7e3db3cd45de59b
Wiseluster <chinaware@foxmail.com>
2016-05-25 19:47:01 +0800

    Adjust Test.java

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test
 {
 	public static void main(String[] args) throws Exception
 	{
-		System.out.println("CPUID-gcj library test");
+		System.out.println("CPUID-GCJ test");
 		System.out.println("CPU vendor ID:\t" + CPUID.getVendorID());
 		System.out.println("CPU brand string:\t" + CPUID.getBrandString());
 	}
